5ac22be067f70810048abc92eb0d2e970e6bdcf6
acidi <acidi@noreply.com>
2014-05-18 15:04:37 +0300

    small cleanups

--- main.lua
+++ main.lua
@@ -4,31 +4,7 @@ require "Ships/ship"
 require "Ships/playership"
 require "Ships/enemyship"
 
-RENDER_SCALE = {}
-DIMENSIONS = {}
-grid = {}
-
-function calculate_grid()
-	mid_line = { {DIMENSIONS.w / 1.8, 0}, {DIMENSIONS.w / 1.8, DIMENSIONS.h} }
-	mid_to_right = { {DIMENSIONS.w / 1.8, DIMENSIONS.h / 2,}, {DIMENSIONS.w, DIMENSIONS.h / 2} }
-	return {mid_line, mid_to_right }
-end
-
-function calc_scale()
-	RENDER_SCALE.x = love.graphics.getWidth()/DIMENSIONS.w
-	RENDER_SCALE.y = love.graphics.getHeight()/DIMENSIONS.h
-end
-
-function draw_line(t)
-	love.graphics.line(t[1][1],t[1][2],t[2][1],t[2][2])
-end
-
 function love.load()
-	-- get window dimensions
-	DIMENSIONS.w = love.graphics.getWidth() 
-	DIMENSIONS.h = love.graphics.getHeight()
-	
-	grid = calculate_grid()
 	love.graphics.setBackgroundColor(3,86,197)
 	playership = Playership:new({canvas = love.graphics.newCanvas(640,720)})
 	enemyship = Enemyship:new({canvas = love.graphics.newCanvas(640,360)})
@@ -53,15 +29,9 @@ function love.load()
 end
 
 function love.update(dt)
-	-- check if window resized and calculate scale
-	if (love.graphics.getHeight ~= DIMENSIONS.h) or (love.graphics.getWidth ~= DIMENSIONS.w) then
-		calc_scale()
-	end
 end
 
 function love.draw()
---	love.graphics.scale(RENDER_SCALE.x,RENDER_SCALE.y) -- scale the game
---	foreach(draw_line, grid)
 	love.graphics.line(640,0,640,720)
 	love.graphics.line(640,360,1280,360)
 	love.graphics.draw(playership:draw())
